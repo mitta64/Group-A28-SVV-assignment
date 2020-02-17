1b5111cdbf88566d757e4afe5b684f29a95070c8
Matthew <matthew23_7@live.nl>
2020-02-17 15:58:52 +0100

    Merged the Cross section tools part and the integrator

--- Code/tools/tools.py
+++ Code/tools/tools.py
@@ -1,20 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Feb 17 15:16:23 2020
-def integral(f,x1,x2,res=10000):
-    i=(x2-x1)/res   # interval
-    A=0
-    a=f(x1)
-    for e in range(res):
-        b=f(x1+(e+1)*i)
-        A+=(a+b)*i/2
-        a=b
-    return A
 
 @author: Group A28 
 """
-
-
+#=======================================================================================
 "Cross sectional properties for bending"
 "Requirement: Make it suitable for a box and an aileron cross section"
 #Compute Centroid
@@ -35,7 +25,7 @@ def integral(f,x1,x2,res=10000):
 
 
 
-
+#=======================================================================================
 "Cross sectional properties for shear"
 "Requirement: Make it suitable for a box and an aileron cross section"
 
@@ -51,3 +41,15 @@ def integral(f,x1,x2,res=10000):
     
 "Encode representation of cross section plus plot"
 
+#============================================================
+"Integrator"
+
+def integral(f,x1,x2,res=10000):
+    i=(x2-x1)/res   # interval
+    A=0
+    a=f(x1)
+    for e in range(res):
+        b=f(x1+(e+1)*i)
+        A+=(a+b)*i/2
+        a=b
+    return A
