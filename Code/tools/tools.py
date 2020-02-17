bcd79de350268a03fe96c2ef9d5aacce9cb5e218
Matthew <matthew23_7@live.nl>
2020-02-17 15:51:05 +0100

    Lay-out of cross sectional properties file
    This file will be part of tools

---
+++ Code/tools/tools.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 17 15:16:23 2020
+
+@author: Group A28 
+"""
+
+
+"Cross sectional properties for bending"
+"Requirement: Make it suitable for a box and an aileron cross section"
+#Compute Centroid
+
+
+
+#Compute Second Moment of Inertia
+
+    #I_xx
+
+
+    #I_yy
+
+    
+    #I_xy
+
+
+
+
+
+
+"Cross sectional properties for shear"
+"Requirement: Make it suitable for a box and an aileron cross section"
+
+#Compute Shear Centre
+    # Requirements:
+        # Locations of the booms
+        # Skin thickness
+        # Skin Locations
+
+
+
+#Compute torsional stiffness
+    
+"Encode representation of cross section plus plot"
