44372f9e0d80b66a7062b2193519dfd3a6426038
Yuri133 <yuri.laar@gmail.com>
2020-02-19 11:59:00 +0100

    Update tools.py

--- Code/tools/tools.py
+++ Code/tools/tools.py
@@ -211,7 +211,7 @@ def integrate_x(x_list):
   return solution
 
 
-"Interpolator"
+"Interpolators 2 different ways: linear of cubic for cubic interpolation 2 boundary conditions are required"
 def spline_coefficient(node,value):
     # IMPORTANT: needs a grid in chronological order (from small to big)
     #This function creates a matrix containing all the splines coefficients for every node,
@@ -250,6 +250,69 @@ def spline_interpolator(Splinematrixx, node, inter_node):
     si = a + b*(inter_node-c)
     return si
 
+def cubicM_matrix(node,value,boundary1,boundary2):
+    # IMPORTANT: needs a grid in chronological order (from small to big)
+    #This function creates a matrix containing all the splines coefficients for every node,
+    #This way the main calculation only has to be done once, and spline_interpolator actually computes the value
+    #input: nodes (1d list), value at these nodes (1dlist), boundary 1 (f'(0)=?),boundary 2 (f'(n)=?)
+    #output Array containing Splinematrix
+
+    Mmatrix = []
+    dmatrix = []
+    Lambda0 = 1
+    Mmatrix.append(list(np.concatenate((np.array([2,Lambda0]),np.zeros(len(node)-2)),axis=0)))
+    dmatrix.append((((value[1]-value[0])-boundary1)/(node[1]-node[0]))/(node[1]-node[0]))
+    for i in range(1,len(node)-1):
+        #Main matrix
+        hi      =  node[i]-node[i-1]
+        hi1     =  node[i+1]-node[i]
+        mui     = hi/(hi+hi1)
+        Lambdai =  1-mui
+        a       = np.zeros(i-1)
+        if len(node)-len(a)-3>=0:
+            b   = np.zeros(len(node)-len(a)-3)
+        else:
+            b   = np.array([])
+        Mmatrix.append(list(np.concatenate((a,np.array([mui,2,Lambdai]),b),axis=0)))
+        #outcome matrix
+        f = ((value[i+1]-value[i])/(node[i+1]-node[i])-(value[i]-value[i-1])/(node[i]-node[i-1]))/(node[i+1]-node[i-1])
+        dmatrix.append(f)
+    mun=1
+    Mmatrix.append(list(np.concatenate((np.zeros(len(node) - 2),np.array([mun,2])), axis=0)))
+    dmatrix.append((boundary2-(value[-1] - value[-2]) / (node[-1] - node[-2])) / (node[-1] - node[-2]))
+    #solve for coefficients
+    dmatrix = 6*np.array(dmatrix)
+    coefficients = np.linalg.solve(Mmatrix,dmatrix)
+    print(coefficients)
+    return coefficients
+
+def cubic_interpolator(Mmatrixx, node, value, inter_value):
+    # This function actually interpolates (1 point)
+    # input Mmatrix from previous function, all nodes (1d array), intervalue (the point to be interpolated)
+    nodenumber=0
+    for i in node:
+        if inter_value<= i:  #inter_value>node[-2]: #check at which spline to interpolate
+            break
+        if inter_value >= node[-2]:
+            nodenumber = len(node)-1
+            break
+        else:
+            nodenumber+=1
+    nodenumber = nodenumber #no
+    xi = node[nodenumber]
+    x_i = node[nodenumber-1]
+    yi = value[nodenumber]
+    y_i = value[nodenumber-1]
+    hi = xi - x_i
+
+    a = Mmatrixx[nodenumber-1]/(6*hi)
+    b =  Mmatrixx[nodenumber]/(6*hi)
+    c = Mmatrixx[nodenumber-1]*hi*hi/6
+    d = Mmatrixx[nodenumber]*hi*hi/6
+    si = a*(xi-inter_value)**3+b*(inter_value-x_i)**3+(y_i-c)*(xi-inter_value)/hi+(yi-d)*(inter_value-x_i)/hi
+    return si
+
+#=====================================================================
 def plot(data, thing_to_plot, unit):
   """ Plot deflection or twist data on a 2D graph
         thing_to_plot and unit should be written as strings, like 'deflection', 'm' '"""
