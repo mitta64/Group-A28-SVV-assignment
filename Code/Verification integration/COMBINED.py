1c9fc811f6b1329a7eef993768eec68d171961a0
Dries Borstlap <d.borstlap@student.tudelft.nl>
2020-02-20 17:05:39 +0100

    Replacement of class

--- Code/Verification integration/COMBINED.py
+++ Code/Verification integration/COMBINED.py
@@ -15,34 +15,32 @@ res = 1000
 
 
 #------------------ main program ---------------------------
-resolution = [5000]
-for res in resolution:
-    start_time = time.time()
+start_time = time.time()
 
-    def function(x):
-        return spline_interpolator(matrix, nodes, x)
+def function(x):
+    return spline_interpolator(matrix, nodes, x)
 
-    nodes = np.linspace(y1,y2,len(grid))
-    solution = []
-    for row in grid:
-        matrix = spline_coefficient(nodes, row)
-        a = def_integral(function,y1,y2,res)
-        solution.append(a)
+nodes = np.linspace(y1,y2,len(grid))
+solution = []
+for row in grid:
+    matrix = spline_coefficient(nodes, row)
+    a = def_integral(function,y1,y2,res)
+    solution.append(a)
 
-    for i in range(n-1):
-        nodes = np.linspace(x1,x2,len(solution))
-        matrix = spline_coefficient(nodes, solution)
-        solution = indef_integral(function,x1,x2,res)
+for i in range(n-1):
+    nodes = np.linspace(x1,x2,len(solution))
+    matrix = spline_coefficient(nodes, solution)
+    solution = indef_integral(function,x1,x2,res)
 
-    #solution = def_integral(function,x1,x2,res)
+solution = def_integral(function,x1,x2,res)
 
-    end_time = time.time()
-    print(res, end_time - start_time, solution)
+end_time = time.time()
+print(res, end_time - start_time, solution)
 
-    ##x = np.linspace(0,1.611,len(solution_n))
-    ##plt.xlabel('x-axis')
-    ##plt.ylabel('z-axis')
-    ##plt.plot(x,solution_n)
-    ##plt.show()
+##x = np.linspace(0,1.611,len(solution_n))
+##plt.xlabel('x-axis')
+##plt.ylabel('z-axis')
+##plt.plot(x,solution_n)
+##plt.show()
 
 
