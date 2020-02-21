b9ba5d2533af1c6544e8c1a3c55757bf715cb046
Dries Borstlap <d.borstlap@student.tudelft.nl>
2020-02-21 11:31:54 +0100

    FInished combined (integral+interpolation+get data) file. Made the integration into a single function. Added descriptions. Also changed the testing file, everything works out now.

--- Code/Verification integration/testing.py
+++ Code/Verification integration/testing.py
@@ -6,31 +6,32 @@ import matplotlib.cm as cm
 
 from data import aero_data, grid, f100, transpose
 from integrator import def_integral, indef_integral
-from interpolator import spline_coefficient, spline_interpolator
+from interpolator import spline_coefficient, spline_interpolator, cubic_coefficient, cubic_interpolator
 
 #--------------------- input data --------------------------------
 
 test_grid = []
 for z in range(81):
-    z=z*15/81
+    z=z*15/80
     row=[]
     for x in range(41):
-        x=x*5/41
+        x=x*5/40
         w = (1+x+x*x)*z
         row.append(w)
     test_grid.append(row)
 grid=transpose(test_grid)
 
+res = 10000
+
+
 x1,x2 = 0,5
 z1,z2 = 0,15
-
 #------------------ main program ---------------------------
-res = 1000
 
 def function(x):
     return spline_interpolator(matrix, nodes, x)
 
-nodes = np.linspace(z1,z2,len(grid))
+nodes = np.linspace(z1,z2,len(grid[0]))
 solution = []
 for row in grid:
     matrix = spline_coefficient(nodes, row)
@@ -38,9 +39,14 @@ for row in grid:
     solution.append(a)
 
 
-##nodes = np.linspace(x1,x2,len(solution))
-##matrix = spline_coefficient(nodes, solution)
-##solution = def_integral(function,x1,x2,res)
+nodes = np.linspace(x1,x2,len(solution))
+matrix = spline_coefficient(nodes, solution)
+solution = def_integral(function,x1,x2,res)
+
+
+
+print(solution)
+
 
 
 ##coord_sys=(0,5,0,15)
@@ -48,10 +54,12 @@ for row in grid:
 ##plt.colorbar()
 ##plt.show()
 
-x = np.linspace(0,5,len(solution))
+
+x = np.linspace(x1,x2,len(solution))
 plt.xlabel('x-axis')
 plt.ylabel('z-axis')
 plt.plot(x,solution)
+plt.grid()
 plt.show()
 
 
