68936716cf94f19f032fd935cd9218ed5db21d26
Yuri133 <yuri.laar@gmail.com>
2020-02-21 15:45:28 +0100

    toolsupdate

--- qforcecoordinates.py
+++ qforcecoordinates.py
@@ -21,6 +21,7 @@ def _coord(length, theta, theta_next):
 # Coordinates of the aerodynamic load
 la = 1.611
 Ca = 0.505
+stepsize = 0.01
 zcoord = [] #chordwise - 81 elements
 for i in np.arange(1,81):
     zcoord.append(_coord(Ca, _theta(i, 41), _theta(i+1,41)) )
@@ -43,4 +44,44 @@ for line in lines:
     qforces.append(spanwise)
     
 qforces = np.array(qforces) #chord*span (z*x) = (81*41)
+
+
+
+#interpolation
+gridx = np.arange(0,la+stepsize,stepsize)
+gridz = np.concatenate((np.arange(0, 0.505,stepsize/10),np.arange(0.505, 0,-stepsize/100)))
+gridzlower = np.arange(0, 0.505,stepsize/10)
+gridzupper = np.arange(stepsize, 0.505,stepsize/10)
+Qforce_interpolated = []
+
+#print(zcoord)
+Qftranspose = np.transpose(qforces)
+print(len(Qftranspose[0]))
+ZQinterpol  = []
+for zvalue in Qftranspose:
+    Intermatrixzl = tl.cubic_coefficients(zcoord[:41], zvalue[:41])
+    Intermatrixzu = tl.cubic_coefficients(list(reversed(zcoord[41:])), list(reversed(zvalue[41:])))
+    Qsinglezlower = []
+    Qsinglezupper = []
+    for zcor in gridzlower:
+        intervalue = tl.cubic_interpolator(Intermatrixzl, zcoord[:41], zvalue[:41], zcor)
+        Qsinglezlower.append(intervalue)
+    for zcor in gridzupper:
+        intervalue = tl.cubic_interpolator(Intermatrixzu, list(reversed(zcoord[41:])), list(reversed(zcoord[41:])), zcor)
+        Qsinglezupper.append(intervalue)
+    #Qsinglezupper = list(reversed(Qsinglezupper))
+    ZQinterpol.append(np.hstack((np.array(Qsinglezlower),np.array(Qsinglezupper))))
+
+
+
+ZQinterpol = np.transpose(ZQinterpol)
+print(len(ZQinterpol))
+for valuex in ZQinterpol:
+    Intermatrix = tl.cubic_coefficients(xcoord,valuex)
+    Qsingle = []
+    for xcor in gridx:
+        intervalue = tl.cubic_interpolator(Intermatrix,xcoord, valuex, xcor)
+        Qsingle.append(intervalue)
+    Qforce_interpolated.append(Qsingle)
+
     
