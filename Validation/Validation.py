cbadefee2e76cc3bebd1c2a71bc320d1214f6b41
Yuri133 <yuri.laar@gmail.com>
2020-02-27 15:03:53 +0100

    Update Validation.py

--- Validation/Validation.py
+++ Validation/Validation.py
@@ -2,6 +2,136 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
+def nodenumberfun(node,node_i):
+    for i in node:
+        nodenumber = 0
+        if node_i<= i:  #inter_value>node[-2]: #check at which spline to interpolate
+            break
+        if node_i >= node[-2]:
+            nodenumber = len(node)-1
+            break
+        else:
+            nodenumber+=1
+    return nodenumber
+
+def cubic_coefficients(node,value):
+    # IMPORTANT: needs a grid in chronological order (from small to big)
+    #This function creates a matrix containing all the splines coefficients for every node,
+    #This way the main calculation only has to be done once, and spline_interpolator actually computes the value
+    #input: nodes (1d list), value at these nodes (1dlist), boundary 1 (f'(0)=?),boundary 2 (f'(n)=?)
+    #output Array containing Splinematrix
+    #boundary1 = (value[1]-value[0])/(node[1]-node[0])
+    #boundary2 = (value[-1]-value[-2])/(node[-1]-node[-2])
+    # print(boundary1,boundary2)
+    Mmatrix = []
+    dmatrix = []
+    Lambda0 = 1
+    #boundary 1
+    Mmatrix.append(list(np.concatenate((np.array([2,0]),np.zeros(len(node)-2)),axis=0)))
+    dmatrix.append(0)#(((value[1]-value[0])-boundary1)/(node[1]-node[0]))/(node[1]-node[0]))
+    for i in range(1,len(node)-1):
+        #Main matrix
+        hi      =  node[i]-node[i-1]
+        hi1     =  node[i+1]-node[i]
+        mui     = hi/(hi+hi1)
+        Lambdai =  1-mui
+        a       = np.zeros(i-1)
+        if len(node)-len(a)-3>=0:
+            b   = np.zeros(len(node)-len(a)-3)
+        else:
+            b   = np.array([])
+        Mmatrix.append(list(np.concatenate((a,np.array([mui,2,Lambdai]),b),axis=0)))
+        #outcome matrix
+        f = ((value[i+1]-value[i])/(node[i+1]-node[i])-(value[i]-value[i-1])/(node[i]-node[i-1]))/(node[i+1]-node[i-1])
+        dmatrix.append(f)
+    mun=1
+    #boundary
+    Mmatrix.append(list(np.concatenate((np.zeros(len(node) - 2),np.array([0,2])), axis=0)))
+    dmatrix.append(0)#(boundary2-(value[-1] - value[-2]) / (node[-1] - node[-2])) / (node[-1] - node[-2]))
+    #solve for coefficients
+    dmatrix = 6*np.array(dmatrix)
+    coefficients = np.linalg.solve(Mmatrix,dmatrix)
+    return coefficients
+
+
+def cubic_interpolator(coefficients, node, value, inter_node):
+    # This function actually interpolates (1 point)
+    # input Splinematrix from previous function, all nodes (1d array), intervalue (the point to be interpolated)
+    nodenumber=0
+    for i in node:
+        if inter_node<= i:  #inter_value>node[-2]: #check at which spline to interpolate
+            break
+        if inter_node >= node[-2]:
+            nodenumber = len(node)-1
+            break
+        else:
+            nodenumber+=1
+    nodenumber = nodenumber #no
+    xi = node[nodenumber]
+    x_i = node[nodenumber-1]
+    yi = value[nodenumber]
+    y_i = value[nodenumber-1]
+    hi = xi - x_i
+
+    a = coefficients[nodenumber-1]/(6*hi)
+    b =  coefficients[nodenumber]/(6*hi)
+    c = coefficients[nodenumber-1]*hi*hi/6
+    d = coefficients[nodenumber]*hi*hi/6
+    si = a*(xi-inter_node)**3+b*(inter_node-x_i)**3+(y_i-c)*(xi-inter_node)/hi+(yi-d)*(inter_node-x_i)/hi
+    return si
+
+def interpolate_val(Data, numxentries, nodex, valueentry, inter_node):
+    #COMPUTES DATA VALUE AT GIVEN X SLICE
+    #INPUT DATA (THE TO BE INTERPOLATED ARRAY (E.G. MISES_SHEAR_BENDING),NUMXENTRIES (NUMBER OF NODES PER SLICE), NODEX(WHICH SLICE)
+    #VALUEENTRY(WHICH COLUMN CONTAINS THE TO BE INTERPOLATED DATA), INTERNODE (Y AND Z COORDINATES)
+    #inter_node = np.sqrt(inter_node[0]**2+inter_node[1]**2)
+    test = Data[0+nodex*numxentries:(nodex+1)*numxentries]
+    spar = []
+    upper = []
+    lower = []
+    for i in test:
+        if float(i[3])==0.0:
+            spar.append(list(i))
+        if i[2] <= 0 and i[3] != 0:
+            lower.append(list(i))
+        if i[2] > 0 and i[3] != 0:
+            upper.append(list(i))
+    spar = np.array(spar)
+    upper = np.array(upper)
+    lower = np.array(lower)
+    spar  = spar[spar[:,2].argsort()]
+    upper = upper[upper[:,3].argsort()]
+    lower = lower[lower[:,3].argsort()]
+    print(lower)
+    #chord = np.vstack((np.array(lower),np.array(upper)))
+    chordcoord = [0]
+    dis = 0
+
+    if inter_node[0]==0:
+        inter_nodeint = inter_node[0]
+        cof = cubic_coefficients(spar[2], spar[:, valueentry])
+        Fr = cubic_interpolator(cof, spar[2], spar[:, valueentry], inter_nodeint)
+    else:
+        if inter_node[0]<0 :
+            for j in range(len(lower) - 1):
+                dis = np.sqrt((lower[j + 1, 2] - lower[j, 2]) ** 2 + (lower[j + 1, 3] - lower[j, 3]) ** 2) + dis
+                chordcoord.append(dis)
+            nodenumber = nodenumberfun(lower[:,3], inter_node[1])
+            inter_nodeint = chordcoord[nodenumber] + np.sqrt((inter_node[0]-lower[nodenumber,2])**2+(inter_node[1]-lower[nodenumber,3])**2)
+            cof = cubic_coefficients(chordcoord, lower[:, valueentry])
+            Fr = cubic_interpolator(cof, chordcoord, lower[:, valueentry], inter_nodeint)
+        if inter_node[0]>0 :
+            for j in range(len(upper) - 1):
+                dis = np.sqrt((upper[j + 1, 2] - upper[j, 2]) ** 2 + (upper[j + 1, 3] - upper[j, 3]) ** 2) + dis
+                chordcoord.append(dis)
+            nodenumber = nodenumberfun(lower[:, 3], inter_node[1])
+            inter_nodeint = chordcoord[nodenumber] + np.sqrt((inter_node[0] - lower[nodenumber, 2]) ** 2 + (inter_node[1] - lower[nodenumber, 3]) ** 2)
+            cof = cubic_coefficients(chordcoord, upper[:, valueentry])
+            Fr = cubic_interpolator(cof, chordcoord, upper[:, valueentry], inter_nodeint)
+
+    return Fr
+
+
 
 
 
@@ -82,7 +212,7 @@ Reaction_bending_nodes      = np.hstack((Boundarynode_coordinates,Reaction_bendi
 Reaction_Jam_nodes          = np.hstack((Boundarynode_coordinates,Reaction_Jam_nodes[:,1:]))
 Reaction_straight_nodes     = np.hstack((Boundarynode_coordinates,Reaction_straight_nodes[:,1:]))
 
-print(Displacement_bending[500])
+
 
 #sorting in x: 62 nodes per slice Mises, 61 nodes per slice Displacement, 108 slices
 Mises_shear_bending          = Mises_shear_bending[Mises_shear_bending[:,1].argsort()]
@@ -99,21 +229,28 @@ Reaction_Jam_nodes           = Reaction_Jam_nodes[Reaction_Jam_nodes[:,1].argsor
 Reaction_straight_nodes      = Reaction_straight_nodes[Reaction_straight_nodes[:,1].argsort()]
 
 
+#print(Mises_shear_bending)
+#Data, numxentries, nodex, valueentry, inter_node
+ainterpolate_val(Mises_shear_bending,62,50,4,[-54,-250])
+
+print(a)
+
+
+
 #slice funtions
-"""
+
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 xs = Mises_coordinates[:,1]
 ys = Mises_coordinates[:,2]
 zs = Mises_coordinates[:,3]
-color  = np.transpose(Mises_shear_bending[:,5]/max(Mises_shear_bending[:,5]))
-colour =[]
-for i in color:
-    zzz = [i,0,1-i]
-    colour.append(zzz)
-print(max(color))
-ax.scatter(xs, ys, zs,color = colour)
+#color  = np.transpose(Mises_shear_bending[:,4]/max(Mises_shear_bending[:,4]))
+#colour =[]
+#for i in color:
+  #   colour.append(zzz)
+#print(max(color))
+ax.scatter(xs, ys, zs,c = Mises_shear_bending[:,4])
 
 ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
@@ -121,4 +258,3 @@ ax.set_zlabel('Z Label')
 
 
 plt.show()
-"""
